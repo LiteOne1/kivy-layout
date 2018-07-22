725e63a2fa2bbbf04a3d3e866df357d45433ee55
LiteOne1 <39678787+LiteOne1@users.noreply.github.com>
2018-07-22 21:33:56 +0300

    Add files via upload

--- engine/grab.py
+++ engine/grab.py
@@ -4,12 +4,29 @@ import requests
 
 
 
-html_get = requests.get('https://www.rottentomatoes.com/m/deadpool_2')
-html_get.encoding = 'UTF-8'
+html_rotten = requests.get('https://www.rottentomatoes.com/m/deadpool_2')
+html_insider = requests.get('https://www.movieinsider.com/m14108/deadpool-2')
+#html_get.encoding = 'UTF-8'
 
-soup = BeautifulSoup(html_get.text, 'html.parser')
+soup = BeautifulSoup(html_rotten.text, 'html.parser')
+soup1 = BeautifulSoup(html_insider.text, 'html.parser')
 
 def get_synopsis(html_soup): 
-    synopsis = soup.find('div', id='movieSynopsis')
+    synopsis = soup.find('div', id ='movieSynopsis')
     return synopsis.text
 
+def get_rating(html_soup):
+    rating = soup.find('span', {'class': 'meter-value superPageFontColor'})
+    return rating.text
+
+def get_cast(html_soup1):
+        actors = ''
+        cast = soup1.find_all('b', itemprop = 'actor')
+        for actor in cast:
+            actors += actor.text+', '
+        return actors
+
+
+print(get_synopsis(soup))
+print(get_rating(soup))
+print(get_cast(soup1))
--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import kivy
+#kivy.require('1.10.0')
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.pagelayout import PageLayout
-from kivy.uix.scatterlayout import ScatterLayout
-from kivy.uix.stacklayout import StackLayout
-from engine.grab import get_synopsis, soup # import from external py file
+from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from engine.grab import get_synopsis, soup, soup1, get_rating, get_cast # import from external py file
+
 
 
 Builder.load_file('kv/layout.kv')# load external kv file
 
 
 class Film(BoxLayout):
-    def get_syn(self): 
-        x = get_synopsis(soup)
-        self.ids.movie_description.text = x
+
+    def get_syn(self):
+        self.ids.synopsis.text = get_synopsis(soup)
+        self.ids.cast.text = get_cast(soup1)
+
+
 
 
 class FilmApp(App):
